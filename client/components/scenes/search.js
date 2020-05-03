5eab129198780257e4c8e6de1a2305d775d3aba4
unknown <macbaren124@gmail.com>
2020-05-03 18:14:54 +0300

    UI1

--- client/components/scenes/search.js
+++ client/components/scenes/search.js
@@ -15,15 +15,16 @@ const Search = () => {
     <div>
       <div className="flex items-center justify-center h-screen">
         <div className="bg-yellow-300 font-bold rounded-lg border shadow-lg p-10">
+          <div className="text-black p-2">GitHub UserName</div>
           <input
-            className="text-black p-2"
+            className="text-black p-2 rounded"
             type="text"
-            placeholder="Enter your repo"
+            placeholder="Enter user's name"
             value={repository}
             onChange={(e) => setRepository(e.target.value)}
           />
-          <button className="ml-4 p-2" type="button" onClick={navigateToTheRepo}>
-            GO
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline" type="button" onClick={navigateToTheRepo}>
+            Search
           </button>
         </div>
       </div>
